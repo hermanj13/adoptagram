7409c5fb8e50468a3107850c265e3ac72207348a
Jake <hermanj13@me.com>
2017-05-01 15:28:22 -0400

    login and reg

--- adoptagram/app/controllers/session_controller.rb
+++ adoptagram/app/controllers/session_controller.rb
@@ -2,15 +2,70 @@ class SessionController < ApplicationController
   def index
   end
 
-  def login
-  end
-
-  def register
-  end
-
   def agency
   end
 
+  def login
+    if params[:type] == "agency"
+      if agency = Agency.find_by(email: params[:email])
+      agency = agency.authenticate(params[:password])
+        if agency
+          session[:id] = agency.id
+          session[:type] = "agency"
+          redirect_to "/agency/#{login.id}/dashboard"
+        else
+          flash[:notice] = ["Invalid Password"]
+          redirect_to "/agency"
+        end
+      else
+        flash[:notice] = ["Invalid Email"]
+        redirect_to "/agency"
+      end
+    elsif params[:type] == "user"
+        if user = user.find_by(email: params[:email])
+        user = user.authenticate(params[:password])
+          if user
+            session[:id] = user.id
+            session[:type] = "user"
+            redirect_to "/dashboard"
+          else
+            flash[:notice] = ["Invalid Password"]
+            redirect_to "/"
+          end
+        else
+          flash[:notice] = ["Invalid Email"]
+          redirect_to "/"
+        end
+    end
+  end
+
+  def register
+    if params[:type] == "agency"
+      agency = Agency.new(name: params[:name], username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation], email: params[:email], website: params[:website])
+      if agency.save
+        login = Agency.find_by(username: params[:username])
+        session[:id] = login.id
+        session[:type] = "agency"
+        redirect_to "/agency/#{login.id}/dashboard"
+      else
+        flash[:notice] = agency.errors.full_messages
+        redirect_to '/agency'
+      end
+    elsif params[:type] == "user"
+      user = User.new(username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation], email: params[:email])
+      if user.save
+        login = User.find_by(username: params[:username])
+        session[:id] = login.id
+        session[:type] = "user"
+        redirect_to '/dashboard'
+      else
+        flash[:notice] = user.errors.full_messages
+      end
+    end
+  end
+
   def logout
+    reset_session
+    redirect_to '/'
   end
 end
--- adoptagram/db/schema.rb
+++ adoptagram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328183831) do
+ActiveRecord::Schema.define(version: 20170330201613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170328183831) do
     t.string   "website"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
   create_table "agency_contacts", force: :cascade do |t|
